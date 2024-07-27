52c860b1c037527b7c2b804426868c73072a1b80
Luka <luka.talevski.hn@gmail.com>
2024-07-27 15:26:47 +0800

    Add a template for potions

--- Scenes/House.gd
+++ Scenes/House.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
+@onready var table = $StaticBody2D/CollisionShape2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	table.shape.size.x = get_viewport_rect().size.x
